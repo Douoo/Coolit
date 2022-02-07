e2243b4d8acd4e954ace993fc5fd929a88649bf1
Douoo <biruksewberiso@gmail.com>
2022-02-07 11:35:38 -0800

    Exception handling added

--- lib/screen/loader.dart
+++ lib/screen/loader.dart
@@ -52,3 +52,42 @@ class _LoadingScreenState extends State<LoadingScreen> {
     );
   }
 }
+
+
+class ErrorWidget extends StatelessWidget {
+  const ErrorWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("¯\\_(ツ)_/¯", style: TextStyle(fontSize: 38)),
+            SizedBox(height: 10),
+            Text(
+              "Come on...You know I can't help you with your device location off.\n Try again with your location on...",
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10),
+            OutlinedButton(
+              onPressed: () async {
+                bool location = await Geolocator.isLocationServiceEnabled();
+                if (location) {
+                  Navigator.pushNamed(context, HomePage.route);
+                }
+              },
+              child: Text(
+                'Retry again',
+                style: TextStyle(color: Colors.black),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
